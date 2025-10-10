306032d69ce0488d52762b86dff2ab1865a71908
Piyaphat Liamwilai <auttyzaza@gmail.com>
2025-10-10 11:48:17 +0700

    add readme generate

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 ### POSN COM 🤑🤑🤑
+
+Array
+- 13BT_Histogram.cpp (athens)[athens%5CArray%5C13BT_Histogram.cpp]
+- Mobile Find.cpp (aut)[aut%5CArray%5CMobile%20Find.cpp]
+- Slot Machine.cpp (aut)[aut%5CArray%5CSlot%20Machine.cpp]
+
+Control-Flow Statement
+- 62_Cross Circle.cpp (aut)[aut%5CControl-Flow%20Statement%5C62_Cross%20Circle.cpp]
+- DE_Tritriangle.cpp (aut)[aut%5CControl-Flow%20Statement%5CDE_Tritriangle.cpp]
+- DE_pongya.cpp (aut)[aut%5CControl-Flow%20Statement%5CDE_pongya.cpp]
+- GT_Peatt.cpp (aut)[aut%5CControl-Flow%20Statement%5CGT_Peatt.cpp]
+- KP_Not Found.cpp (aut)[aut%5CControl-Flow%20Statement%5CKP_Not%20Found.cpp]
+- POSNimage.cpp (aut)[aut%5CControl-Flow%20Statement%5CPOSNimage.cpp]
+
+Introduction to Progamming
+- 62_Sphere and Circle.cpp (aut)[aut%5CIntroduction%20to%20Progamming%5C62_Sphere%20and%20Circle.cpp]
+- BT_BTK.cpp (aut)[aut%5CIntroduction%20to%20Progamming%5CBT_BTK.cpp]
+- KP_Khunchai PeaTT.cpp (aut)[aut%5CIntroduction%20to%20Progamming%5CKP_Khunchai%20PeaTT.cpp]
+
---
+++ generate_readme.py
@@ -0,0 +1,46 @@
+import os
+import urllib.parse
+from collections import defaultdict
+
+base_dir = '.'  # repo root
+readme_file = 'README.md'
+
+content = "### POSN COM 🤑🤑🤑\n\n"
+
+# Skip hidden folders
+programmers = [f for f in os.listdir(base_dir)
+               if os.path.isdir(f) and not f.startswith('.')]
+programmers.sort()
+
+# Collect files per topic across all programmers
+topics_dict = defaultdict(list)
+
+for programmer in programmers:
+    programmer_path = os.path.join(base_dir, programmer)
+    topics = [t for t in os.listdir(programmer_path)
+              if os.path.isdir(os.path.join(programmer_path, t)) and not t.startswith('.')]
+    topics.sort()
+    
+    for topic in topics:
+        topic_path = os.path.join(programmer_path, topic)
+        files = [f for f in os.listdir(topic_path)
+                 if os.path.isfile(os.path.join(topic_path, f)) and not f.startswith('.')]
+        files.sort()
+        
+        for file in files:
+            file_path = urllib.parse.quote(os.path.join(programmer, topic, file))
+            topics_dict[topic].append((programmer, file, file_path))
+
+# Write topics in order
+for topic in sorted(topics_dict.keys()):
+    content += f"{topic}\n"
+    
+    # sort by programmer, then file name
+    sorted_files = sorted(topics_dict[topic], key=lambda x: (x[0], x[1]))
+    for programmer, file, file_path in sorted_files:
+        content += f"- {file} ({programmer})[{file_path}]\n"
+    content += "\n"
+
+# Write to README.md
+with open(readme_file, 'w', encoding='utf-8') as f:
+    f.write(content)
