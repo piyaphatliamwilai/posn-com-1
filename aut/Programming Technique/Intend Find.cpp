c16f1e65ba11b7a4a08fe5bd41aa7dfdb14ec351
木 漏 れ 日 <82884260+piyaphatliamwilai@users.noreply.github.com>
2025-10-15 17:48:45 +0700

    Intend Find

---
+++ aut/Programming Technique/Intend Find.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int q[100005];
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int N, Q;
+    cin >> N >> Q;
+    for (int i = 0; i < N; i++) cin >> q[i];
+    sort(q, q+N);
+    while (Q--) {
+        int x;
+        cin >> x;
+        cout << (int)(upper_bound(q, q+N, x) - q) << "\n";
+    }
+}
